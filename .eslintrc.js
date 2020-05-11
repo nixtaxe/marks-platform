c30ab3a64422a5d3728cca4c6c1a26aa600fd24f
nixtaxe <nixtaxe@protonmail.com>
2020-05-11 12:41:33 +1000

    Configure lint and format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
   },
-  'extends': [
-    'plugin:vue/essential',
+  extends: [
+    'plugin:vue/recommended',
+    'plugin:vue-scoped-css/recommended',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    'plugin:@typescript-eslint/eslint-recommended',
+    '@vue/typescript',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    indent: ['error', 2],
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    'space-before-function-paren': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    // Next 2 lines are needed for solving problem with interface imports
+    // Solution source: https://stackoverflow.com/a/61555310
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
 }
