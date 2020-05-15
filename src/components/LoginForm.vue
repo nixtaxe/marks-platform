51cf632d54945ddc93eaf57c0db84d6fc4a6c40e
nixtaxe <nixtaxe@protonmail.com>
2020-05-15 13:28:09 +1000

    Connect LoginForm component and loginFormMachine

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,38 +4,68 @@
       Добро пожаловать!
     </v-card-title>
     <v-card-text>
-      <v-form>
+      <v-alert
+        :value="isError"
+        :outlined="true"
+        type="error"
+      >
+        {{ context.error }}
+      </v-alert>
+      <v-form
+        ref="form"
+        @submit.prevent="onSubmit()"
+      >
         <v-text-field
           v-model="username"
+          name="username"
           label="Имя пользователя"
+          :rules="usernameRules"
+          required
+          :autofocus="true"
+          @change="sendChange({ key: 'username', value: username })"
         />
         <v-text-field
           v-model="password"
+          name="password"
           label="Пароль"
+          type="password"
+          :rules="passwordRules"
+          required
+          @change="sendChange({ key: 'password', value: password })"
         />
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            x-large
+            min-width="150px"
+            color="primary"
+            type="submit"
+            :loading="isSubmitting"
+          >
+            Войти
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
       </v-form>
     </v-card-text>
-    <v-card-actions>
-      <v-spacer />
-      <v-btn
-        x-large
-        min-width="150px"
-        color="primary"
-      >
-        Войти
-      </v-btn>
-      <v-spacer />
-    </v-card-actions>
   </v-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import { Prop } from 'vue-property-decorator'
 
-@Component
+import useLoginFormMachine from '@/compositions/useLoginFormMachine'
+
+@Component({
+  setup (props: any) {
+    return { ...useLoginFormMachine(props.loginFormMachine) }
+  },
+})
 export default class LoginForm extends Vue {
-  username: String = ''
-  password: String = ''
+  @Prop() loginFormMachine: any
+  username: string = ''
+  password: string = ''
 }
 </script>
