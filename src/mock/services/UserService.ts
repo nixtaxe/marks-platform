3c64639d5acdc3122407875f7445403fb15b2221
nixtaxe <nixtaxe@protonmail.com>
2020-05-15 13:23:55 +1000

    Change login mock for error testing

--- src/mock/services/UserService.ts
+++ src/mock/services/UserService.ts
@@ -3,16 +3,23 @@ import IUserService from '@/services/IUserService'
 import TokenResponse from '@/models/TokenResponse'
 import token from '@/mock/data/token.json'
 
-const load = <T>(mockData: T, time = 0) => {
-  return new Promise<T>((resolve) => {
+const load = <T>(mockData: T, time = 0, error: string | null = null) => {
+  return new Promise<T>((resolve, reject) => {
     setTimeout(() => {
-      resolve(mockData)
+      if (error === null) resolve(mockData)
+      else reject(error)
     }, time)
   })
 }
 
 export default class FakeUserService implements IUserService {
   public login (username: string, password: string): Promise<TokenResponse> {
-    return load<TokenResponse>(token, 1000)
+    if (username === 'username' && password === 'password')
+      return load<TokenResponse>(token, 1000)
+    return load<TokenResponse>(
+      token,
+      1000,
+      'Неправильное имя пользователя или пароль',
+    )
   }
 }
