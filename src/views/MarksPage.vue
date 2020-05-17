4d2ab8831e84e391725fe5afb52fc04b4e2eb925
nixtaxe <nixtaxe@protonmail.com>
2020-05-18 09:29:48 +1000

    Add empty marks page view

---
+++ src/views/MarksPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Marks Page</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({})
+export default class MarksPage extends Vue {}
+</script>
