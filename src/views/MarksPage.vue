30a550e7ebbd69e24c53706ace83b06b00d6827f
nixtaxe <nixtaxe@protonmail.com>
2020-05-18 16:07:06 +1000

    Connect marks page view to state machine

--- src/views/MarksPage.vue
+++ src/views/MarksPage.vue
@@ -1,15 +1,22 @@
 <template>
-  <ToolBar />
+  <ToolBar
+    v-if="isLoaded"
+    :toolbar-machine="toolbarMachine"
+  />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import ToolBar from '@/layouts/ToolBar.vue'
+import useMarksPageMachine from '../compositions/useMarksPageMachine'
 
 @Component({
   components: {
     ToolBar,
+  },
+  setup () {
+    return useMarksPageMachine()
   },
 })
 export default class MarksPage extends Vue {}
