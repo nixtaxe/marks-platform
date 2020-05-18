c8777b14b1c43cb0d035d9cfb8c07730c391e7b4
nixtaxe <nixtaxe@protonmail.com>
2020-05-18 15:59:39 +1000

    Add use marks page machine

---
+++ src/compositions/useMarksPageMachine.ts
@@ -0,0 +1,10 @@
+import { useMachine } from '@xstate/vue'
+import marksPageMachine from '@/state-machines/MarksPageMachine'
+import { computed } from '@vue/composition-api'
+
+export default function useMarksPageMachine () {
+  const { state } = useMachine(marksPageMachine)
+  const isLoaded = computed(() => state.value.matches('loaded.main'))
+  const toolbarMachine = computed(() => state.value.children.toolbarMachine)
+  return { isLoaded, toolbarMachine }
+}
