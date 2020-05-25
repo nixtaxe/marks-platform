7de547e29830cc34c2371331146820d4ad243b8c
nixtaxe <nixtaxe@protonmail.com>
2020-05-25 18:53:18 +1000

    Add apollo client

---
+++ src/services/HttpClient.ts
@@ -0,0 +1,30 @@
+import ApolloClient from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:1337/graphql',
+})
+
+const middlewareLink = setContext(async () => {
+  const userInfo = await localStorage.getItem('user-info')
+  const token = JSON.parse(userInfo || '{}').jwt
+  if (token === undefined) return {}
+  else
+    return {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+})
+
+const link = middlewareLink.concat(httpLink)
+
+const apolloClient = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
+
+export default apolloClient
